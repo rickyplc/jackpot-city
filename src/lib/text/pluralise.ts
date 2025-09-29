8432d2881ae72e3c14a4ab4a71537831fc080756
Ricky <r@gemba.me>
2025-09-29 13:20:58 +0200

    Adds a pluralisation JSDoc description

--- src/lib/text/pluralise.ts
+++ src/lib/text/pluralise.ts
@@ -1,3 +1,11 @@
+/**
+ * Pluralises a word based on the count.
+ *
+ * @param count - The number of items.
+ * @param singular - The singular form of the word.
+ * @param plural - The plural form of the word.
+ * @returns The plural form of the word.
+ */
 export const pluralise = (count: number, singular: string, plural?: string) => {
   return count === 1 ? singular : (plural ?? `${singular}s`)
 }
